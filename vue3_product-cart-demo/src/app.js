65b20fdaa1d9be2ed3af3ec941bd79acb47ea7f3
Ankit bhardwaj <101131763+monster0freason@users.noreply.github.com>
2023-08-12 18:51:28 +0530

    Add files via upload

---
+++ vue3_product-cart-demo/src/app.js
@@ -0,0 +1,123 @@
+let app = Vue.createApp({
+    data() {
+      return {
+        showHome: true,
+        showProducts: false,
+        showSidebar: false,
+        inventory: [],
+        cart: {}
+      }
+    },
+    computed: {
+      totalQuantity() {
+        return Object.values(this.cart).reduce((acc, curr) => {
+          return acc + curr
+        }, 0)
+      }
+      
+    },
+    methods: {
+      addToCart(name, index) {
+        if (!this.cart[name]) this.cart[name] = 0
+        this.cart[name] += this.inventory[index].quantity
+        console.log(this.cart)
+        this.inventory[index].quantity = 0          },
+      toggleSidebar(){
+        this.showSidebar =!this.showSidebar
+      },
+      removeItem(name) {
+        delete this .cart[name]
+      },
+      openHome() {
+        this.showHome = true
+        this.showProducts = false
+      },
+      openProducts() {
+        this.showHome = false
+        this.showProducts = true
+      },
+      openHistory() {
+        this.showHome = false
+        this.showProducts = false
+      },
+
+    },
+    async mounted() {
+      const res =await fetch('./food.json')
+      const data = await res.json()
+      this.inventory = data
+    }
+  })
+
+  app.component('sidebar',{
+    props: ['toggle','cart','inventory', 'remove'],
+    computed: {
+      
+    },
+    methods: {
+      getPrice(name) {
+        const product = this.inventory.find((p) => {
+          return p.name === name
+        })
+        return product.price.USD
+      },
+      calculateTotal() {
+        const total = Object.entries(this.cart).reduce((acc, curr, index) => {
+          return acc + (curr[1] * this.getPrice(curr[0]))
+        },0)
+        return total.toFixed(2)
+      }
+    },
+    template:`
+    <aside class="cart-container">
+    <div class="cart">
+      <h1 class="cart-title spread">
+        <span>
+          Cart
+          <i class="icofont-cart-alt icofont-1x"></i>
+        </span>
+        <button @click="toggle" class="cart-close">&times;</button>
+      </h1>
+
+      <div class="cart-body">
+        <table class="cart-table">
+          <thead>
+            <tr>
+              <th><span class="sr-only">Product Image</span></th>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Qty</th>
+              <th>Total</th>
+              <th><span class="sr-only">Actions</span></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(quantity, key, i) in cart" :key="i">
+              <td><i class="icofont-carrot icofont-3x"></i></td>
+              <td>{{ key }}</td>
+              <td>\${{ getPrice(key) }}</td>
+              <td class="center">{{ quantity }}</td>
+              <td>\${{quantity * getPrice(key)}}</td>
+              <td class="center">
+                <button @click="remove(key)" class="btn btn-light cart-remove">
+                  &times;
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <p class="center" v-if="!Object.keys(cart).length"><em>No items in cart</em></p>
+        <div class="spread">
+          <span><strong>Total:</strong> \${{ calculateTotal() }}</span>
+          <button class="btn btn-light">Checkout</button>
+        </div>
+      </div>
+    </div>
+  </aside>
+    `
+
+  })
+  
+
+  app.mount('#app')
